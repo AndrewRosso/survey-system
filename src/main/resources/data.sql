6bb5245aeec280feef1b1fced39493c0389c5dae
AndrewRosso <a.krasnov91@mail.ru>
2022-04-13 02:09:27 +0300

    init: add H2db & spring security configure

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO SURVEY (id, name, start_Date, end_Date, description)
+VALUES (1,'Про работу', '2022-04-13','2022-05-13','здесь будет описание');
+INSERT INTO SURVEY (id, name, start_Date, end_Date, description)
+VALUES (2,'Про машину', '2022-04-13','2022-06-13','здесь будет описание');
