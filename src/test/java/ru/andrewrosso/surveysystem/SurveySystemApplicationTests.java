2d11704a70ebee4e9d3a7e2e4a874a19a6defa97
AndrewRosso <a.krasnov91@mail.ru>
2022-04-13 00:22:57 +0300

    initial commit

---
+++ src/test/java/ru/andrewrosso/surveysystem/SurveySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andrewrosso.surveysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
